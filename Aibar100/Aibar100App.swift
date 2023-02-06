5228c1d8d1311173ee4b116e50e93f6d8c60b4fb
test <astappleid@itstep.kz>
2023-02-06 15:43:07 +0600

    initial commit

---
+++ Aibar100/Aibar100App.swift
@@ -0,0 +1,17 @@
+//
+//  Aibar100App.swift
+//  Aibar100
+//
+//  Created by Swift on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Aibar100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
